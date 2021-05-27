9b7f032fc3435ff579a522e578caba3130d64a11
Angad Tendulkar <angad.tendulkar@gmail.com>
2021-05-27 12:43:02 +0000

    Revert

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,4 +1,5 @@
 var input = document.querySelector('#url');
+var ginput = document.querySelector('#gurl');
 
 input.addEventListener('keyup', (key) => {
     if (key.keyCode == 13) { 
@@ -10,6 +11,15 @@ input.addEventListener('keyup', (key) => {
 
 document.querySelector("#initiate").addEventListener('click', () => {
 
+    if (!input.value.trim().length) return;
+    window.location.assign(`/prox/?url=${btoa(input.value)}`);
+
+});
+
+document.querySelector("#gsearch").addEventListener('click', () => {
+
+	ginput.replace(" ", "+")
+	input = `https://google.com/search?q=${ginput}`
     if (!input.value.trim().length) return;
     window.location.assign(`/prox/?url=${btoa(input.value)}`);
 
