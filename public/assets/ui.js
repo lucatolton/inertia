69c9166d6d879e99043fa3452e8f4b6f79e74208
Angad Tendulkar <angad.tendulkar@gmail.com>
2021-09-14 20:03:52 +0000

    Settings Overlay

---
+++ public/assets/ui.js
@@ -0,0 +1,7 @@
+function settingsOverlay() {
+	document.getElementById("settingsOverlay").style.display = "block";
+}
+
+function disableSettingsOverlay() {
+	document.getElementById("settingsOverlay").style.display = "none";
+}
