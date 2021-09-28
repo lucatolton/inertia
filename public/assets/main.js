5a7a3854dfbd75bc3091f8a06ecd2b207207626c
TheAlphaReturns <angad.tendulkar@gmail.com>
2021-09-28 15:27:53 -0400

    Things

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,9 +1,16 @@
-document.querySelector("#formWrap").addEventListener("submit", e => {
+const config = require("./config.json");
+
+document.querySelector("#formWrap").addEventListener("submit", (e) => {
 	e.preventDefault();
-	const data = new FormData(e.target)
+	const data = new FormData(e.target);
 	if (!data.get("url")) return;
-	
-	const param = e.target.id === "gsearch" ? 
-		`https://google.com/search?q=${data.get("url").trim().replace(" ", "+")}` :	data.get("url");
-		window.location.assign(`/prox/?url=${btoa(param)}`);
-})
+
+	const param =
+		e.target.id === "gsearch"
+			? `https://google.com/search?q=${data
+					.get("url")
+					.trim()
+					.replace(" ", "+")}`
+			: data.get("url");
+	window.location.assign(`/${config.prefix}/?url=${btoa(param)}`);
+});
